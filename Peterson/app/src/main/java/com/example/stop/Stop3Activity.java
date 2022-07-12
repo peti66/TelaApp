fa13f9bd63bccf23401e07df2ad5c9cd7b0f329e
peti66 <92984800+peti66@users.noreply.github.com>
2022-07-12 10:07:32 -0300

    version 1

---
+++ Peterson/app/src/main/java/com/example/stop/Stop3Activity.java
@@ -0,0 +1,14 @@
+package com.example.stop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Stop3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stop3);
+    }
+}
